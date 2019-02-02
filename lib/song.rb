dbb3fc385b6c8985599c123ea9a9c8f99f224e04
DANIELLE PARK <dpark1008@gmail.com>
2019-02-02 01:48:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,12 @@
 require 'pry'
 
 class Song
+  attr_accessor :name
+  attr_reader :artist
   extend Memorable
   extend Findable
   include Paramable
-
-  attr_accessor :name
-  attr_reader :artist
-
+  
   @@songs = []
 
   def initialize
